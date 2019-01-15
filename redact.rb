4081e43a3a5fb8ebe7dc34d8578bb9e1c7054d4a
Pauline Rudge <paulinerudge@me.com>
2019-01-15 21:45:34 +0000

    added some comments so that the look was explained more thouroughl

--- redact.rb
+++ redact.rb
@@ -12,7 +12,9 @@ redact_words = redact.split(" ")
 sentence = ""
 
 words.each do |word|
+  # redact_words.include? word - returns true if word in sentence is one of the words to be redacted,
+  # else is returns false.
   sentence = sentence + ( (redact_words.include? word) ? "REDACTED" : word) + " "
-end
+  end
 
 print sentence
